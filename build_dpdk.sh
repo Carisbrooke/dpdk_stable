166a5bb325722693ffe9f847c4cf7c271df05a33
repu1sion <bearrailgun@gmail.com>
2018-07-18 11:41:31 +0100

    improved build script

--- build_dpdk.sh
+++ build_dpdk.sh
@@ -6,11 +6,16 @@ sed -ri 's,(CONFIG_RTE_BUILD_SHARED_LIB=).*,\1y,' .config
 sed -ri 's,(CONFIG_RTE_LIBRTE_PMD_PCAP=).*,\1y,' .config
 sed -ri 's,(CONFIG_RTE_LIBRTE_MLX5_PMD=).*,\1y,' .config
 cd ..
-make -j7 install T=x86_64-native-linuxapp-gcc EXTRA_CFLAGS="-fPIC" DESTDIR=/usr/local
+make -j12 install T=x86_64-native-linuxapp-gcc EXTRA_CFLAGS="-fPIC" DESTDIR=/usr/local
 if [ $? -eq 0 ]; then 
 	ldconfig
 	echo "build ok"
 else
 	echo  "build failed"
 fi
+
+#create symbolic link to headers for odp
+cd x86_64-native-linuxapp-gcc/include
+ln -s ../include/ dpdk
+cd ../../
 
