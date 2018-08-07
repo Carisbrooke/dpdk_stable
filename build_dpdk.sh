f4d274418430192227b1dd807e05791598886e1f
repu1sion <bearrailgun@gmail.com>
2018-08-07 21:35:21 +0100

    updated dpdk build script, now can run with number of jobs as param

--- build_dpdk.sh
+++ build_dpdk.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+JOBS=12
+
+if [ $# -eq 1 ]; then
+	echo "setting jobs to cmdline param"
+	JOBS=$1
+fi
+
+echo "jobs number is: $JOBS"
+
 make config T=x86_64-native-linuxapp-gcc O=x86_64-native-linuxapp-gcc
 cd x86_64-native-linuxapp-gcc/
 sed -ri 's,(CONFIG_RTE_BUILD_SHARED_LIB=).*,\1y,' .config
 sed -ri 's,(CONFIG_RTE_LIBRTE_PMD_PCAP=).*,\1y,' .config
 sed -ri 's,(CONFIG_RTE_LIBRTE_MLX5_PMD=).*,\1y,' .config
 cd ..
-make -j12 install T=x86_64-native-linuxapp-gcc EXTRA_CFLAGS="-fPIC" DESTDIR=/usr/local
+make -j${JOBS} install T=x86_64-native-linuxapp-gcc EXTRA_CFLAGS="-fPIC" DESTDIR=/usr/local
 if [ $? -eq 0 ]; then 
 	ldconfig
 	echo "build ok"
